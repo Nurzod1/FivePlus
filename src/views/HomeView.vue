44318eee488045ae76c7ed5bf23da0075577debc
Nurzod1 <nurzodkaramanov1@gmail.com>
2023-04-14 13:17:15 +0500

    init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="h1">
+    <h1>hello</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.h1 {
+  h1 {
+    color: red;
+  }
+}
+</style>

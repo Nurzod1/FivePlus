85075dfacdad3b100ac076d5be4da5b0642794fd
rakhimmmmm23 <84928945+rakhimmmmm23@users.noreply.github.com>
2023-04-20 19:18:39 +0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import views from '@/components/UI'
+import views from '@/views/UI'
 
 
 const app = createApp(App)
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { useAuthStore } from '../stores/auth/AuthStore';
-import Login from "../components/Login.vue";
-import Profile from "../components/Profile.vue";
-import Work from "../components/Work.vue";
-import MyChilds from "../components/MyChilds.vue";
-import History from '../components/History.vue'
-import Support from '../components/Support.vue'
-import Balance from '../components/Balance.vue'
-import Role from '../components/Role.vue'
-import Chat from '../components/Chat.vue'
-import Settings from '../components/Settings.vue'
-import Teacher from '../components/Teacher.vue'
-import Register from '../components/Register.vue'
-
+import Login from "../views/Login.vue";
+import Profile from "../views/Profile.vue";
+import Work from "../views/Work.vue";
+import MyChilds from "../views/MyChilds.vue";
+import History from '../views/History.vue'
+import Support from '../views/Support.vue'
+import Balance from '../views/Balance.vue'
+import Role from '../views/Role.vue'
+import Chat from '../views/Chat.vue'
+import Settings from '../views/Settings.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,93 +19,25 @@ const router = createRouter({
       component: Login,
     },
     {
-      path: "/",
+      path: "/profile",
       name: "Profile",
       component: Profile,
-      meta: {
-        requiresAuth: true,
-        roles: ['parent', 'teacher', 'pupil'],
-      },
-      beforeEnter: (to, from, next) => {
-        const authStore = useAuthStore();
-        const role = authStore.role;
-        const requiredRoles = to.meta.roles;
-  
-        if (!authStore.user) {
-          next({ path: '/login' });
-        } else if (!requiredRoles || requiredRoles.includes(role)) {
-          next();
-        } else {
-          next({ path: '/403' });
-        }
-      },
     },
     {
       path: "/work",
       name: "Work",
       component: Work,
-      meta: {
-        requiresAuth: true,
-        roles: ['teacher'],
-      },
-      beforeEnter: (to, from, next) => {
-        const authStore = useAuthStore();
-        const role = authStore.role;
-        const requiredRoles = to.meta.roles;
-  
-        if (!authStore.user) {
-          next({ path: '/login' });
-        } else if (!requiredRoles || requiredRoles.includes(role)) {
-          next();
-        } else {
-          next({ path: '/403' });
-        }
-      },
-      
     },
     {
       path: "/mychilds",
       name: "MyChilds",
       component: MyChilds,
-      meta: {
-        requiresAuth: true,
-        roles: ['parent'],
-      },
-      beforeEnter: (to, from, next) => {
-        const authStore = useAuthStore();
-        const role = authStore.role;
-        const requiredRoles = to.meta.roles;
-  
-        if (!authStore.user) {
-          next({ path: '/login' });
-        } else if (!requiredRoles || requiredRoles.includes(role)) {
-          next();
-        } else {
-          next({ path: '/403' });
-        }
-      },
     },
+ 
     {
       path: "/history",
       name: "History",
       component: History,
-      meta: {
-        requiresAuth: true,
-        roles: ['parent', 'pupil', 'teacher'],
-      },
-      beforeEnter: (to, from, next) => {
-        const authStore = useAuthStore();
-        const role = authStore.role;
-        const requiredRoles = to.meta.roles;
-  
-        if (!authStore.user) {
-          next({ path: '/login' });
-        } else if (!requiredRoles || requiredRoles.includes(role)) {
-          next();
-        } else {
-          next({ path: '/403' });
-        }
-      },
     },
     {
       path: "/support",
@@ -135,16 +63,6 @@ const router = createRouter({
       path: "/settings",
       name: "Setting",
       component: Settings,
-    },
-    {
-      path: "/teacher",
-      name: "Teacher",
-      component: Teacher,
-    },
-    {
-      path: "/register",
-      name: "Register",
-      component: Register,
     },
   ],
 });
